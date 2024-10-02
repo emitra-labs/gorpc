f528a9cfc7d17aa242c4fa16de74ba3204c57689
Ukasyah <hi.ukasyah@gmail.com>
2024-10-02 11:27:26 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/emitra-labs/gorpc
+
+go 1.23.1
+
+require (
+	github.com/emitra-labs/common v1.0.0
+	github.com/sethvargo/go-envconfig v1.1.0
+	google.golang.org/grpc v1.67.1
+)
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.22.1 // indirect
+	github.com/iancoleman/strcase v0.3.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240930140551-af27646dc61f // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
---
+++ gorpc.go
@@ -0,0 +1,99 @@
+package gorpc
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/emitra-labs/common/log"
+	"github.com/emitra-labs/common/validator"
+	"github.com/sethvargo/go-envconfig"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/reflection"
+)
+
+type Server struct {
+	GRPC *grpc.Server
+}
+
+type Config struct {
+	Port int `env:"GORPC_PORT, default=4000"`
+}
+
+var config *Config
+var server *Server
+
+func init() {
+	config = new(Config)
+
+	// Load config from environment variables
+	err := envconfig.Process(context.Background(), config)
+	if err != nil {
+		panic(err)
+	}
+
+	// Validate config
+	err = validator.Validate(config)
+	if err != nil {
+		panic(err)
+	}
+
+	server = &Server{
+		GRPC: grpc.NewServer(),
+	}
+	healthCheck := health.NewServer()
+	grpc_health_v1.RegisterHealthServer(server.GRPC, healthCheck)
+	reflection.Register(server.GRPC)
+}
+
+func GetGRPCServer() *grpc.Server {
+	return server.GRPC
+}
+
+func Start() error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", config.Port))
+	if err != nil {
+		panic(err)
+	}
+
+	log.Infof("gRPC server is listening at %s", lis.Addr())
+
+	return server.GRPC.Serve(lis)
+}
+
+func Shutdown() error {
+	server.GRPC.GracefulStop()
+
+	return nil
+}
+
+func NewTest(server *grpc.Server) (clientConn *grpc.ClientConn, closer func()) {
+	lis, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		log.Errorf("Failed to listen localhost:0: %s", err)
+	}
+
+	go func() {
+		if err := server.Serve(lis); err != nil {
+			log.Errorf("Error serving test server: %s", err)
+		}
+	}()
+
+	clientConn, err = grpc.NewClient(
+		lis.Addr().String(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Errorf("Failed to connect to server: %s", err)
+	}
+
+	closer = func() {
+		server.GracefulStop()
+		lis.Close()
+	}
+
+	return clientConn, closer
+}
